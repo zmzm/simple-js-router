aa03eccb9d1749a1b743b24e25cad8cc5cce4fd8
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-06-08 23:51:33 +0300

    listening to navigation changes

--- src/router.js
+++ src/router.js
@@ -5,6 +5,8 @@ class Router {
 
   #paths = {};
 
+  #previousPath = null;
+
   #init() {
     document.addEventListener(
       'click',
@@ -16,9 +18,12 @@ class Router {
 
     window.addEventListener('DOMContentLoaded', (event) => {
       event.preventDefault();
+      this.#checkPatterns(window.location.pathname);
     });
 
-    window.addEventListener('popstate', () => {});
+    window.addEventListener('popstate', () => {
+      this.#checkPatterns(window.location.pathname);
+    });
   }
 
   #parsePattern(pattern) {
@@ -26,6 +31,76 @@ class Router {
     const step2 = step1.replace(/(*|:w*)/gm, '(:?[A-z0-9|*]*)');
 
     return `^${step2}(?:\/?)$`;
+  }
+
+  #checkIfAnyPatternIsMatching(url) {
+    return (element) => {
+      const pattern = this.#paths[element].reg;
+      const newRegex = new RegExp(pattern, 'i').exec(url);
+
+      if (!newRegex) {
+        return false;
+      }
+
+      return true;
+    };
+  }
+
+  #transformRegexOutput(input) {
+    try {
+      return Object.keys(input)
+        .filter((key) => Number(key))
+        .reduce((obj, key) => {
+          return { ...obj, [key]: input[key] };
+        }, {});
+    } catch (error) {
+      // Return 404 page
+      return {};
+    }
+  }
+
+  #getParams(pattern, url) {
+    const parsedPattern = this.#paths[pattern].reg;
+    const regex = new RegExp(parsedPattern, 'i');
+
+    const tokens = this.#transformRegexOutput(regex.exec(pattern));
+    const params = this.#transformRegexOutput(regex.exec(url));
+
+    return [...tokens, ...params];
+  }
+
+  #processURL(pattern, url) {
+    this.#previousPath = url;
+    const result = this.#getParams(pattern, url);
+
+    return this.#paths[pattern].callbacks.forEach((callback) =>
+      callback(result)
+    );
+  }
+
+  #checkPatterns(url) {
+    const targetUrl = url;
+
+    // Find the matching pattern
+    const result = Object.keys(this.#paths).find(
+      this.#checkIfAnyPatternIsMatching(targetUrl)
+    );
+
+    // If pattern is not found we return an error or 404
+    if (!result) {
+      // Return 404 page
+      return false;
+    }
+
+    // Prevent reloading the page if the url is the same
+    if (this.#previousPath === targetUrl) {
+      return false;
+    }
+
+    // Run the callback function
+    this.#processURL(result, targetUrl);
+
+    return true;
   }
 
   // registers path
